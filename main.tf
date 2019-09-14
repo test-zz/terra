948fe93a66d4fa40ef282da42334392f43a129a2
Ewan <ewann@users.noreply.github.com>
2019-09-14 22:14:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,20 +4,24 @@ variable "project" {
 provider "google" {
   project = var.project
   region  = "europe-west4"
-  zone    = "europe-west-4d"
+  zone    = "europe-west4-c"
 }
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "n1-standard-1"
   boot_disk {
     initialize_params {
-      image = "gce-uefi-images/ubuntu-1804-bionic-v20190813a"
+      image = "ubuntu-os-cloud/ubuntu-1804-bionic-v20190813a"
     }
   }
   network_interface {
     # A default network is created for all GCP projects
-    network       = "default"
+    network       = "${google_compute_network.vpc_network.self_link}"
     access_config {
     }
   }
+}
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
 }
