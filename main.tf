e68ce2c40a610b72e5da1095e563c59c6c258637
Ewan <ewann@users.noreply.github.com>
2019-09-15 00:50:34 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,14 +6,26 @@ provider "google" {
   region  = "europe-west4"
   zone    = "europe-west4-c"
 }
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "n1-standard-1"
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-1804-bionic-v20190813a"
-    }
-  }
+
+resource "google_compute_instance" "vm_instance" {                                                                                                                             
+  name                    = "terraform-instance"                                                                                                                               
+  machine_type            = "n1-standard-1"                                                                                                                                    
+  metadata = {                                                                                                                                                                 
+    ssh-keys = "${var.ssh_user}:${file("${var.ssh_filename}")}"  
+  }                                                                                                                                                                            
+
+  provisioner "file" {                                                                                                                                                         
+    source      = var.script_path                                                                                                                                              
+    destination = "/tmp/metadata_startup_script.sh"                                                                                                                            
+  }                                                                                                                                                                            
+                                                                                                                                                                               
+  provisioner "remote-exec" {                                                                                                                                                  
+    inline = [                                                                                                                                                                 
+      "/tmp/metadata_startup_script.sh",                                                                                                                                       
+      "echo end of remote-exec",                                                                                                                                               
+    ]                                                                                                                                                                          
+  } 
+
   network_interface {
     # A default network is created for all GCP projects
     network       = "${google_compute_network.vpc_network.self_link}"
