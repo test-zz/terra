98170531bf40a77348caa5fe6e1454c26ce06a07
Ewan <ewann@users.noreply.github.com>
2019-09-15 06:12:30 +0100

    fix typo

--- setup.sh
+++ setup.sh
@@ -6,18 +6,24 @@ if [ ! -f ~/.vimrc ]; then
   echo 'colo evening' >> ~/.vimrc
   echo 'syntax on' >> ~/.vimrc
   echo 'set backspace=indent,eol,start' >> ~/.vimrc
+else
+  echo Skipped .vimrc update
 fi
 
 if [ ! -f ~/.ssh/$(whoami) ]; then
   echo Generating ssh key...
   ssh-keygen -t rsa -f ~/.ssh/$(whoami) -C $(whoami) -q -N ""
+else
+  echo Skipped ssh-keygen
 fi
 
 terraform init
 
 pushd ..
-if [ ! -f jenkins-x-workshop ]; then
+if [ ! -e jenkins-x-workshop ]; then
   git clone https://github.com/dcanadillas/jenkins-x-workshop.git
   #https://github.com/cloudbees-days/jenkins-x-workshop
+else
+  echo Skipped git clone
 fi
 popd
