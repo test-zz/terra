d331e26ec3210a3303fe8f3674a61f3c03ff9c97
Ewan <ewann@users.noreply.github.com>
2019-09-15 04:07:17 +0100

    add jenkinsx workshop

--- setup.sh
+++ setup.sh
@@ -5,3 +5,8 @@ echo 'syntax on' >> ~/.vimrc
 echo 'set backspace=indent,eol,start' >> ~/.vimrc
 terraform init
 ssh-keygen -t rsa -f ~/.ssh/$(whoami) -C $(whoami) -q -N ""
+
+pushd ..
+git clone https://github.com/dcanadillas/jenkins-x-workshop.git
+#https://github.com/cloudbees-days/jenkins-x-workshop
+popd
